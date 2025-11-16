3b86bab6fa209b665bdc72d521ab27a2a7a935a3
Galaktischer Imperator Malarius VIII <mariushoffmann2004@gmail.com>
2025-11-16 17:45:54 +0100

    feat(lernset): Endpunkte mit Funktionalität gefüllt.

--- src/backend/app/routers/learnsets.py
+++ src/backend/app/routers/learnsets.py
@@ -9,29 +9,32 @@ router = APIRouter(
     tags=["learning_set"]
 )
 
-#create new learning set
 @router.post("/")
 def create_learning_set(learning_set: LearningSetBase, session: Session = Depends(get_session))-> LearningSet:
+    """Adds a new learning set to DB"""
+
     print("create_learning_set called")
+
+    db_learning_set = LearningSet.model_validate(learning_set)
+    session.add(db_learning_set)
+    session.commit()
+    session.refresh(db_learning_set)
+
+    return db_learning_set
 
 @router.get("/")
 def get_learning_sets(session: Session = Depends(get_session))-> list[LearningSetResponse]:
-    print("here, you get all learning sets")
+    """Returns list of all learning sets in DB"""
+
     return session.exec(select(LearningSet)).all()
 
-#TO DO
 @router.delete("/{id}")
-def delete_learning_set() -> bool:
-    return True
+def delete_learning_set(id: int, session: Session = Depends(get_session)) -> bool:
+    """Deletes a learning set from DB"""
+    db_learning_set = session.get(LearningSet,id)
 
+    if not db_learning_set:
+        raise HTTPException(status_code = 404, detail="Learning set not found")
 
-#get new question in learning set id
-@router.get("/{id}/questions")
-def get_learning_set_question():
-    print("get_learning_set_question called")
-    #TO DO
-
-#create new answer in learning set id
-#@router.post("/{id}/answer")
-#def create_learning_set_answer():
-#    print("create_learning_set_answer called")
+    session.delete(db_learning_set)
+    session.commit()

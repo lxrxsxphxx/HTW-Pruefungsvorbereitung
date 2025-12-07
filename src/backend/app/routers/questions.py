41700c7f2f423bc9ca3688d1fd092b5048ea1ce6
Galaktischer Imperator Malarius VIII <mariushoffmann2004@gmail.com>
2025-12-07 17:58:07 +0100

    docs(routers/*.py): added/changed docstrings and changed return values

--- src/backend/app/routers/questions.py
+++ src/backend/app/routers/questions.py
@@ -17,6 +17,17 @@ router = APIRouter(
 
 @router.post("/", response_model=QuestionResponse)
 def create_question(question: QuestionBase, answers: list[AnswerBase],learning_set_id:int, session: Session = Depends(get_session)) -> QuestionResponse:
+    """
+    Docstring for create_question
+    
+    Args:
+        question (QuestionBase): The Question that is going to be added
+        answers (list[AnswerBase]): the possible answers a user can give
+        learning_set_id (int): the id of the learning set the question should belong to
+        session (Session): the database session
+    Returns:
+        QuestionResponse: The Question that has been added
+    """
     db_question = Question.model_validate(question)
 
     db_learning_set=session.get(LearningSet,learning_set_id)
@@ -38,10 +49,31 @@ def create_question(question: QuestionBase, answers: list[AnswerBase],learning_s
 
 @router.get("/")
 def read_questions(session: Session = Depends(get_session)) -> list[QuestionResponse]:
+    """
+    Reads all questions from the database
+    
+    Args:
+        session (Session): the database session
+    
+    Returns:
+        list[QuestionResponse]: All questions in the database
+    """
+
     return session.exec(select(Question)).all()
 
 @router.get("/{id}")
 def read_question(id: int, session: Session = Depends(get_session)) -> QuestionResponse:
+    """
+    Reads a single question by its id
+    
+    Args:
+        id (int): the id of the wanted question
+        session (Session): the database session
+    
+    Returns:
+        QuestionResponse: The question that is stored
+    """
+
     question = session.get(Question, id)
     if not question:
         raise HTTPException(status_code=404, detail="Question not found")
@@ -49,6 +81,18 @@ def read_question(id: int, session: Session = Depends(get_session)) -> QuestionR
 
 @router.put("/{id}")
 def update_question(id: int, question: QuestionBase, session: Session = Depends(get_session)) -> QuestionResponse:
+    """
+    update a question, but not its answers!
+    
+    Args:
+        id (int): the id of the question taht is going to be updated:
+        question (QuestionBase): the new information of the question 
+        session (Session): the database session
+    
+    Returns:
+        QuestionResponse: the updated question
+    """
+
     db_question = session.get(Question, id)
     if not db_question:
         raise HTTPException(status_code=404, detail="Question not found")
@@ -61,6 +105,14 @@ def update_question(id: int, question: QuestionBase, session: Session = Depends(
 
 @router.delete("/{id}")
 def delete_question(id: int, session: Session = Depends(get_session)):
+    """
+    deletes a question
+    
+    Args:
+        id (int): the id of the question that is going to be deleted
+        session (Session): the database session
+    """
+
     question = session.get(Question, id)
     if not question:
         raise HTTPException(status_code=404, detail="Question not found")
@@ -70,6 +122,18 @@ def delete_question(id: int, session: Session = Depends(get_session)):
 
 @router.put("/answers/{id}")
 def update_answer(id: int, answer: AnswerBase ,session: Session = Depends(get_session)) -> AnswerResponse:
+    """
+    Updates an answer
+    
+    Args:
+        id (int): the id of the answer that is going to be updated
+        answer (AnswerBase): the new information of the answer
+        session (Session): the database session
+    
+    Returns:
+        AnswerResponse: the updated answer
+    """
+
     db_answer = session.get(Answer, id)
     if not db_answer:
         raise HTTPException(status_code=404, detail="Answer not found")

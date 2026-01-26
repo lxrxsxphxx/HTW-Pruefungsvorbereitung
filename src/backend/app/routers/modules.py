f3caec99744c41df6c243a4e86fa276178013e8f
Frederic Egle <cynihx@gmail.com>
2026-01-26 20:18:55 +0100

    feat(modules.py): add add_module_to_user, remove_module_from_user

--- src/backend/app/routers/modules.py
+++ src/backend/app/routers/modules.py
@@ -5,7 +5,7 @@ This file describes the REST endpoint for database interactions with modules.
 from fastapi import Depends, APIRouter, HTTPException
 from sqlmodel import Session, select
 
-from app.models import ModuleBase, Module, ModuleResponse, Course, CourseResponse, CourseModule
+from app.models import ModuleBase, Module, ModuleResponse, Course, CourseResponse, CourseModule, User, ModuleUser
 from app.dependencies import get_session, validate_jwt
 
 router = APIRouter(
@@ -75,3 +75,62 @@ def get_courses(id:int,session:Session = Depends(get_session),
                            .join(CourseModule)
                            .where(CourseModule.course_id == id)).all()
     return courses
+
+@router.post("/{id}/user")
+def add_module_to_user(id: int, session:Session = Depends(get_session),
+                username:str = Depends(validate_jwt)):
+    """
+    Handles adding a module to a users profile
+    
+    Args:
+        id (int): the id of the module
+        session (Session): the database session
+        username (str): Username of the current user - extracted from jwt
+    
+    Returns:
+        null
+    """
+    db_module = session.get(Module, id)
+    if not db_module:
+        raise HTTPException(status=404, detail="This module does not exist")
+    
+    db_user = session.exec(select(User).where(User.username == username)).all()
+    if not db_user or len(db_user)>1:
+        raise HTTPException(status = 500,detail="something has gone terribly wrong")
+    
+    db_modeluser = session.get(ModuleUser, (db_module.id, db_user[0].id))
+    if db_modeluser:
+        raise HTTPException(status=400, detail="This module is already assigned to the user")
+    
+    module_user = ModuleUser(module_id=db_module.id, user_id=db_user[0].id)
+    session.add(module_user)
+    session.commit()
+
+@router.delete("/{id}/user")
+def remove_module_from_user(id: int, session:Session = Depends(get_session),
+                username:str = Depends(validate_jwt)):
+    """
+    Handles removing a module from a users profile
+    
+    Args:
+        id (int): the id of the module
+        session (Session): the database session
+        username (str): Username of the current user - extracted from jwt
+    
+    Returns:
+        null
+    """
+    db_module = session.get(Module, id)
+    if not db_module:
+        raise HTTPException(status=404, detail="This module does not exist")
+    
+    db_user = session.exec(select(User).where(User.username == username)).all()
+    if not db_user or len(db_user)>1:
+        raise HTTPException(status = 500,detail="something has gone terribly wrong")
+    
+    db_modeluser = session.get(ModuleUser, (db_module.id, db_user[0].id))
+    if not db_modeluser:
+        raise HTTPException(status=404, detail="This module is not assigned to the user")
+    
+    session.remove(db_modeluser)
+    session.commit()
